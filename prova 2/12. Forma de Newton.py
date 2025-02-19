beec10d2bb41c7bcddce8f547bcdc427bc77d7f8
Rogerio <rogeriodosanjosbarbosa2004@gmail.com>
2025-02-19 10:05:35 -0300

    Adicionando o algoritmo da forma de Newton, com numpy e matplot

---
+++ prova 2/12. Forma de Newton.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def FormaDeNewton(x_tab, y_tab):
+    """
+    Calcula o polinômio interpolador usando a forma de Newton
+    
+    Parâmetros:
+    x_tab -- array numpy com os pontos x de interpolação
+    y_tab -- array numpy com os valores correspondentes y
+    
+    Retorna:
+    coeficientes -- array com os coeficientes do polinômio de Newton
+    """
+    n = len(x_tab)
+    coeficientes = np.zeros(n)
+    
+    # Calcula as diferenças divididas
+    for i in range(n):
+        if i == 0:
+            coeficientes[i] = y_tab[i]
+        else:
+            # Calcula a diferença dividida de ordem i
+            numerador = y_tab[i] - FormaDeNewton(x_tab[:i+1], y_tab[:i+1])[i-1]
+            denominador = np.prod(x_tab[i] - x_tab[:i])
+            coeficientes[i] = numerador / denominador
+            
+    return coeficientes
+
+def avalia_newton(x, x_tab, coeficientes):
+    """
+    Avalia o polinômio de Newton em um ponto x
+    
+    Parâmetros:
+    x -- ponto onde avaliar o polinômio
+    x_tab -- array com os pontos x originais
+    coeficientes -- array com os coeficientes calculados
+    
+    Retorna:
+    valor do polinômio em x
+    """
+    resultado = coeficientes[0]
+    for i in range(1, len(coeficientes)):
+        termo = coeficientes[i]
+        for j in range(i):
+            termo *= (x - x_tab[j])
+        resultado += termo
+    return resultado
+
+# Exemplo de uso
+x_tab = np.array([1., 2., 3., 4.])
+y_tab = np.array([2., 3., 5., 7.])
+
+# Calcula os coeficientes
+coeficientes = FormaDeNewton(x_tab, y_tab)
+
+# Cria pontos para plotagem suave
+x_plot = np.linspace(min(x_tab)-0.5, max(x_tab)+0.5, 1000)
+y_plot = np.array([avalia_newton(x, x_tab, coeficientes) for x in x_plot])
+
+# Plotagem
+plt.figure(figsize=(10, 6))
+plt.plot(x_plot, y_plot, 'b-', label='Polinômio de Newton')
+plt.scatter(x_tab, y_tab, color='red', marker='o', s=100, 
+           label='Pontos de Interpolação')
+plt.grid(True)
+plt.legend()
+plt.title('Interpolação Polinomial de Newton')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+print("Coeficientes do polinômio de Newton:")
+print(coeficientes)
